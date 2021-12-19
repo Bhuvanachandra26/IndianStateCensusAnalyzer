0abd34372a29194f9d0f9c27844cea51786aeb7b
Bhuvanachandra26 <bhuvanachandra26@gmail.com>
2021-12-19 12:56:08 +0530

    Added : Loading Indian States Code Information from csv file and added some test cases

---
+++ src/main/java/com/bridgelabz/indianstatecensusanalyzer/Exceptions/CustomizedExceptions.java
@@ -0,0 +1,11 @@
+/**
+ * Purpose : To create customized Exception
+ */
+
+package com.bridgelabz.indianstatecensusanalyzer.Exceptions;
+
+public class CustomizedExceptions extends Exception {
+    public CustomizedExceptions(String message) {
+        super(message);
+    }
+}
